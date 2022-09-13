7ca3e38bcef9f5b835c82d9d17c8f7c5fab8c3f8
Pavel Gaidukov <mailgpn@gmail.com>
2022-09-13 20:37:35 +0500

    workflow test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,33 +62,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
-
-# if DEBUG:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": os.getenv(
-#                 "DB_ENGINE", default="django.db.backends.postgresql"
-#             ),
-#             "NAME": os.getenv("DB_NAME", default="postgres"),
-#             "USER": os.getenv("POSTGRES_USER", default="postgres"),
-#             "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-#             "HOST": os.getenv("DB_HOST", default="postgres"),
-#             "PORT": os.getenv("DB_PORT", default=5432),
-#         }
-#     }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.getenv(
+                "DB_ENGINE", default="django.db.backends.postgresql"
+            ),
+            "NAME": os.getenv("DB_NAME", default="postgres"),
+            "USER": os.getenv("POSTGRES_USER", default="postgres"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
+            "HOST": os.getenv("DB_HOST", default="postgres"),
+            "PORT": os.getenv("DB_PORT", default=5432),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
